32b663933cb62a8e5b8d4ccf8cf70acb83c0cae5
Dmitriy Zhiganov <delawere2009@yandex.ru>
2021-10-29 16:31:01 +0300

    fix context error

--- src/components/nav.js
+++ src/components/nav.js
@@ -18,14 +18,18 @@ const navItems = [
 
 const Nav = () => {
     const path = typeof window !== 'undefined' ? window.location.href : '';
-    const url = new URL(path)
+    let pathname = '';
+    if (path) {
+      const url = new URL(path)
+      pathname = url.pathname
+    }
 
     return (
       <nav className="header-nav">
         <ul className="header-nav-list">
           {navItems.map(({ title, link }) => (
             <li key={title} className="header-nav-item">
-              <Link className={`${url.pathname === link ? "header-link-disabled" : ""}`} disabled={url.pathname === link} to={link}>{title}</Link>
+              <Link className={`${pathname === link ? "header-link-disabled" : ""}`} disabled={pathname === link} to={link}>{title}</Link>
             </li>
           ))}
         </ul>
